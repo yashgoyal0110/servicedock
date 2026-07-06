672d2e6d45cd85f0b6e9708938e76c88a2dc1b30
yashgoyal0110 <yg364550@gmail.com>
2026-07-06 21:26:39 +0530

    client: add entry point and global styles

---
+++ client/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import { App } from './App'
+import { AuthProvider } from './context/auth'
+import './index.css'
+
+const rootEl = document.getElementById('root')
+if (!rootEl) {
+  throw new Error('Root element #root not found')
+}
+
+createRoot(rootEl).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
